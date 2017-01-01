2d5305fea7ade7caf7083fd4575b620efd45c56a
Nathan Baker <nbaker@ieee.org>
2016-12-31 19:44:43 -0800

    Testing reading/writing from strings

--- test.py
+++ test.py
@@ -4,25 +4,60 @@ import tempfile
 import todo
 import datetime as dt
 
-_TEST_GOOD_STRING = "(A) 2017-03-02 This string is good @better @best @great"
-_TEST_BAD_STRING = "This string is bad"
+_STRING_VARIABLES = {
+    "priority" : ["A", "Q", None],
+    "date" : [dt.date(2016, 1, 2), dt.date(2017, 3, 2), None],
+    "context" : [["bar", "foo"], ["pi", "31451"], ["fnord"], []],
+    "title" : ["This is a test todo item"]
+}
 
 class TestTodo(unittest.TestCase):
     """ Test todo module functionality """
     def setUp(self):
         """ Set up directories, etc. """
         self.temp_dir = tempfile.TemporaryDirectory()
-        self.good_item = todo.TodoItem(_TEST_GOOD_STRING)
-        self.bad_item = todo.TodoItem(_TEST_BAD_STRING)
+        self.test_objects = []
+        for priority in _STRING_VARIABLES["priority"]:
+            for date in _STRING_VARIABLES["date"]:
+                for contexts in _STRING_VARIABLES["context"]:
+                    for title in _STRING_VARIABLES["title"]:
+                        item = todo.TodoItem()
+                        item.priority = priority
+                        item.due_date = date
+                        item.contexts = contexts
+                        item.title = title
+                        self.test_objects.append(item)
     def test_setup(self):
         """ Test setup of package directories, etc. """
         todo.setup_folders(self.temp_dir.name)
-    def test_todo_str(self):
-        """ Test extraction of features from a todo string """
-        self.assertEqual(self.good_item.priority, "A", msg="Checking priority")
-        self.assertIsNone(self.bad_item.priority, msg="Checking priority")
-        self.assertEqual(self.good_item.due_date, dt.date(2017, 3, 2), msg="Checking due date")
-        self.assertIsNone(self.bad_item.priority, msg="Checking due date")
+    def test_null_title(self):
+        """ Test title = None """
+        test_item = self.test_objects[0]
+        test_item = todo.TodoItem(str(test_item))
+        test_item.title = None
+        with self.assertRaises(todo.TodoError):
+            todo_str = str(test_item)
+    def do_test(self, attr):
+        """" Test attribute loading from string """
+        for item in self.test_objects:
+            todo_str = str(item)
+            test_item = todo.TodoItem(todo_str)
+            item_attr = getattr(item, attr)
+            test_attr = getattr(test_item, attr)
+            test_message = "Testing %s (%s, %s)" % (attr, item_attr, test_attr)
+            self.assertEqual(item_attr, test_attr)
+    def test_priority(self):
+        """ Test priorities """
+        self.do_test("priority")
+    def test_dates(self):
+        """ Test due dates """
+        self.do_test("due_date")
+    def test_contexts(self):
+        """ Test contexts """
+        self.do_test("contexts")
+    def test_title(self):
+        """ Test title """
+        self.do_test("title")
     def tearDown(self):
         self.temp_dir.cleanup()
 
--- todo.py
+++ todo.py
@@ -4,8 +4,9 @@ import os.path
 import os
 import re
 
-PRIORITY_RE = re.compile("\(([a-zA-Z])\)")
-DATE_RE = re.compile("([0-9]{4})-([0-9]{2})-([0-9]{2})")
+PRIORITY_RE = re.compile("\(([a-zA-Z])\) *")
+DATE_RE = re.compile("([0-9]{4})-([0-9]{2})-([0-9]{2}) *")
+CONTEXTS_RE = re.compile("(@.*)")
 
 TODAY = dt.date.today()
 _DATA_PATH = "data"
@@ -28,10 +29,15 @@ def setup_folders(path):
         folder_path = os.path.join(path, folder)
         os.makedirs(name=folder_path, exist_ok=True)
 
+class TodoError(Exception):
+    """ Error for Todo class """
+    pass
+
 class TodoItem:
     """ A todo item """
     def __init__(self, todo_str=None):
         """ Set up todo item with an optional todo string """
+        self.title = None
         self.priority = None
         self.contexts = []
         self.due_date = None
@@ -40,6 +46,23 @@ class TodoItem:
         self.todo_str = None
         if todo_str:
             self.parse_todo_str(todo_str)
+    def __str__(self):
+        """ Return the todo string for this item """
+        strings = []
+        if self.completed:
+            strings.append("x")
+        if self.priority:
+            strings.append("({})".format(self.priority))
+        if self.due_date:
+            strings.append("{}".format(self.due_date.strftime("%Y-%m-%d")))
+        if self.title:
+            strings.append(self.title)
+        else:
+            errstr = "Todo item is missing title!"
+            raise TodoError(errstr)
+        for context in self.contexts:
+            strings.append("@" + context)
+        return " ".join(strings)
     def parse_todo_str(self, todo_str):
         """ Parse a todo item formatted as a string """
         self.todo_str = todo_str
@@ -47,19 +70,32 @@ class TodoItem:
         match = PRIORITY_RE.search(_str)
         try:
             self.priority = match.expand("\g<1>").upper()
-            print(math.span)
+            span = match.span()
+            _str = _str[span[1]:]
         except AttributeError:
             self.priority = None
         try:
-            match = DATE_RE.search(todo_str)
+            match = DATE_RE.search(_str)
             year = int(match.group(1))
             month = int(match.group(2))
             day = int(match.group(3))
             self.due_date = dt.date(year, month, day)
+            span = match.span()
+            _str = _str[span[1]:]
         except AttributeError:
             self.due_date = None
-
-
+        try:
+            match = CONTEXTS_RE.search(_str)
+            span = match.span()
+            self.title = _str[:span[0]].strip()
+            contexts = match.group(1)
+            for context in contexts.split("@"):
+                context = context.strip()
+                if context:
+                    self.contexts.append(context)
+        except AttributeError:
+            self.title = _str.strip()
+            _str = ""
 
 
 if __name__ == "__main__":
